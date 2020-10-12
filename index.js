13559d339aa9c1c8ad9ca0f9ad697d45021178ee
Hez <69835689+heznanda@users.noreply.github.com>
2020-10-12 16:35:25 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const mysql=require('mysql');
+const express=require('express');
+const bodyparser = require('body-parser');
+
+var app = express();
+app.use(bodyparser.json());
+
+var mysqlConnection = mysql.createConnection({
+	host:'localhost',
+	user:'hezkiel',
+	password:'hezkiel',
+	database:'sitepoint'
+});
+
+mysqlConnection.connect((err)=>{
+	if(!err)
+		console.log('DB connection successful!');
+	else
+		console.log('DB connection error: '+JSON.stringify(err,undefined,2));
+});
+
+app.listen(3000,()=>console.log('express server is running at port 3000'));
+
+app.get('/employees',(req,res)=>{
+	mysqlConnection.query('SELECT * FROM authors',(err,rows,fields)=>{
+		if(!err)
+			res.send(rows);
+		else
+			console.log(err);
+	})
+});
+
+app.get('/employees/:id',(req,res)=>{
+	mysqlConnection.query('SELECT * FROM authors a WHERE a.id = ?',[req.params.id],(err,rows,fields)=>{
+		if(!err)
+			res.send(rows);
+		else
+			console.log(err);
+	})
+});
